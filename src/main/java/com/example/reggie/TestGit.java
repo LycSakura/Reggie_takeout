d482a4118ebdbf87c70919dfa1cd641d5c415a46
Ellyina <2978223452@qq.com>
2024-02-02 10:14:01 +0800

    first

---
+++ src/main/java/com/example/reggie/TestGit.java
@@ -0,0 +1,12 @@
+package com.example.reggie;
+
+/***
+ *@title TestGit
+ *@CreateTime 2024/2/2 10:13
+ *@description
+ **/
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+    }
+}
